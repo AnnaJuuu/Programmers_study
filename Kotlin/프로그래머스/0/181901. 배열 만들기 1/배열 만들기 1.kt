821344b5589149e0abb6ae0cd1d3d1e9432c8db7
Anna <167738708+AnnaJuuu@users.noreply.github.com>
2026-07-04 01:30:03 +0900

    [level 0] Title: 배열 만들기 1, Time: 6.66 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(n: Int, k: Int): IntArray {
+        var answer = mutableListOf<Int>()
+        for (i in 1..n) {
+            if (i % k == 0) {
+                answer.add(i)
+            }
+        }
+        
+        return answer.toIntArray()
+    }
+}
